94d292c923dd484f92cd76063d1d43e9f0bad8d4
Blake Washington <bwashington@onemagnify.com>
2022-10-25 10:37:31 -0400

    add README, start spreadsheet work

---
+++ README.md
@@ -0,0 +1,17 @@
+## Purpose
+
+The purpose of this script is to take in a folder as input, list out each sub-folder title as columns, then list out the 
+titles of each folders' contents as rows beneath aforementioned columns. All of this should be displayed on a spreadsheet,
+which is to be automatically downloaded at the end of the writing proccess.
+The following is a visual of how the spreadsheet should look:
+
+  FOLDER_1  | DATE | DIFFICULTY |  FOLDER_2  | DATE | DIFFICULTY | ...
+ --------------------------------------------------------------------
+  CONTENT_1 |      |            | CONTENT_1  |      |            |
+  CONTENT_2 |      |            | CONTENT_2  |      |            |
+     ...    |      |            |    ...     |      |            |
+ 
+## Requirements
+* python version 3.6
+* pip install xlsxwriter
+* user's folder should exist in same directory as this project
--- spreadsheet_algo.py
+++ spreadsheet_algo.py
@@ -1,17 +1,4 @@
-# The purpose of this script is to take in a folder as input, list out each sub-folder title as columns, then list out the 
-# titles of each folders' contents as rows beneath aforementioned columns. All of this should be displayed on a spreadsheet,
-# which is to be automatically downloaded at the end of the writing proccess.
-# The following is a visual of how the spreadsheet should look:
-#
-#  FOLDER_1  | DATE | DIFFICULTY |  FOLDER_2  | DATE | DIFFICULTY | ...
-# --------------------------------------------------------------------
-#  CONTENT_1 |      |            | CONTENT_1  |      |            |
-#  CONTENT_2 |      |            | CONTENT_2  |      |            |
-#     ...    |      |            |    ...     |      |            |
-# 
-# This script was written with python version 3.9 in mind
-
-import os, os.path as op, spreadsheet_algo_functions as saf, time
+import os, os.path as op, spreadsheet_algo_functions as saf, time, xlsxwriter
 
 # get name of folder from user input
 user_folder = input("Enter folder name: ")
@@ -30,6 +17,10 @@ if saf.find_dir(top_dir_list, user_folder):
     # check to see if list has content. If not, exit program
     if sub_dir_list:
         print("Folder found. Generating spreadsheet...")
+
+        # create spreadsheet using xlsxwriter
+        workbook = xlsxwriter.Workbook(user_folder +'.xlsx')
+        
         sub_dir_list.sort()
 
         # iterate through list of sub-directories and its file contents
