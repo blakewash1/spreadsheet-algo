67e4c04fbdd717b293f679fb266fcb0b52905bf9
Blake Washington <bwashington@onemagnify.com>
2022-10-24 21:13:40 -0400

    complete functionality for gathering list of sub-directories in user folder

--- spreadsheet_algo.py
+++ spreadsheet_algo.py
@@ -11,24 +11,33 @@
 # 
 # This script was written with python version 3.9 in mind
 
-import os, spreadsheet_algo_functions as saf, time
+import os, os.path as op, spreadsheet_algo_functions as saf, time
 
 # get name of folder from user input
 user_folder = input("Enter folder name: ")
 
-# check if folder exists in current directory
-dir_path = os.path.dirname(os.path.realpath(__file__))
+# check if folder exists in current directory. If not, exit program
+dir_path = op.dirname(op.realpath(__file__))
 top_dir_list = os.listdir(dir_path)
 if saf.find_dir(top_dir_list, user_folder):
-    print("Folder found. Generating spreadsheet...")
+    # append user folder to dir_path variable
+    dir_path += "\\" + user_folder
+    # create a list of the sub-directories wihin the user folder
+    sub_dir_list = [f for f in os.listdir(dir_path) if not op.isfile(f)]
+    # check to see if list has content. If not, exit program
+    if sub_dir_list:
+        print("Folder found. Generating spreadsheet...")
+    else:
+        print("There are no sub-folders within selected folder. Exiting...")
+    
 else:
-    print("Folder not found") 
+    print("Folder not found. Exiting...") 
 
 # add a short delay
 time.sleep(1)
 # for item in os.listdir(dir_path):
 #     print(item)
-#     print(os.path.isfile(item))
+#     print(op.isfile(item))
 
 # for root, dirs, files in next(os.walk(dir_path))[1]:
 #     for dir in dirs:
