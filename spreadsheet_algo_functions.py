6b2346180ae57495e2282ae63c198ad945d39ee9
Blake Washington <bwashington@onemagnify.com>
2022-10-26 15:37:24 -0400

    create new create_spreadsheet function that takes in a dictionary. Want to make this modular enough to be used for both folders and webpages

--- spreadsheet_algo_functions.py
+++ spreadsheet_algo_functions.py
@@ -8,8 +8,39 @@ def find_dir(dir, name):
             return True
     return False
 
+# takes in a dictionary where key = header name (folder name, week #, etc), and
+# value = the list of contents. Iterates through the dictionary and prints to
+# a new spreadsheet
+def create_spreadsheet(name, dict):
+    # create spreadsheet using xlsxwriter
+    xlsx_path = op.join(os.getcwd(), "find_spreadsheet_here", name +'.xlsx')
+    workbook = xlsxwriter.Workbook(xlsx_path)
+    worksheet = workbook.add_worksheet()
+    cell_format = workbook.add_format({'bold': True, 'bg_color': '#FAFAD2'})
+
+    # widen the length of the first and third column
+    worksheet.set_column(0, 0, 60)
+    worksheet.set_column(2, 2, 30)
+
+    # variable for keeping track of row num as we go down the spreadsheet
+    row = 0
+    for header, content_list in dict:
+        # writing directory-name, date, and difficulty as section headers
+        worksheet.write(row, 0, header, cell_format)
+        worksheet.write(row, 1, "Date", cell_format)
+        worksheet.write(row, 2, "Understanding", cell_format)
+
+        # writing contents to the sheet
+        for content in content_list:
+            row += 1
+            worksheet.write(row, 0, content)
+        # iterating the row number by 2 to create space for the next section
+        row += 2
+           
+    workbook.close()
+
 # for writing the contents of a directory to a spreadsheet (in alpha-numeric order)
-def create_spreadsheet(name, dir_path, dir_list):
+def create_spreadsheet_old(name, dir_path, dir_list):
     # create spreadsheet using xlsxwriter
     xlsx_path = op.join(os.getcwd(), "find_spreadsheet_here", name +'.xlsx')
     workbook = xlsxwriter.Workbook(xlsx_path)
