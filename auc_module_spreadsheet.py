7ae2d5e151c01b899c60b8df1361aa760cca6bf5
Blake Washington <bwashington@onemagnify.com>
2022-10-27 16:05:41 -0400

    complete work on creating dict of weeks, days, and materials

--- auc_module_spreadsheet.py
+++ auc_module_spreadsheet.py
@@ -1,4 +1,5 @@
 import os, os.path as op, spreadsheet_algo_functions as saf, time
+from re import sub
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
 
@@ -16,18 +17,45 @@ if proper_file != "nope":
 
     with open(op.join(dir_path, proper_file)) as file:
         soup = bs(file, "html.parser")
-        # weeks = soup.find_all("span", class_="name")["text"]
-        # print(weeks)
+        # 2D dictionary for containing weeks, days, and materials
+        module_dict = {}
 
-        # get each week's content
+        # get each week
         top_div = soup.find("div", { "id" : "context_modules" })
         weeks = top_div.find_all('div', recursive=False)
         for week in weeks:
-            print(week.find("span", class_="name").get_text())
-            # get each day 
-            days_and_contents = week.find_all("li")
-            for item in days_and_contents:
-                print(item.find("span", {'class':['title', 'locked_title']})["title"])
+            week_text = (week.find("span", class_="name").get_text())
+            # variables needed to create an entry to module_dict
+            sub_dict = {}
+            day = ""
+            materials = []
+
+            # get each day and its materials
+            days_and_materials = week.find_all("li")
+            for item in days_and_materials:
+                # this could either be a day or course material. If a day, make this the key of the dict
+                # otherwise, add it to the materials list, which will be the value of the dict
+                title = item.find("span", {'class':['title', 'locked_title']})["title"]
+                if title.casefold().startswith("day"):
+                    # first, add previous day (if any) and its materials to the dict
+                    if (day):
+                        sub_dict[day] = materials
+                        print(day)
+                        print(materials)
+                    # then start new day w/ new materials
+                    day = title
+                    materials.clear()
+                else:
+                    materials.append(title)
+
+            # add final day of week to sub_dict
+            sub_dict[day] = materials
+            print(day)
+            print(materials)
+            # add completed sub_dict to module_dict
+            module_dict[week_text] = sub_dict
+
+
 
 
 
